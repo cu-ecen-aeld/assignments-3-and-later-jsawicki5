833a37c859c7016585dead8a4ce7b27807d63ecd
jsawicki <jsawicki@Ubuntu.myguest.virtualbox.org>
2024-07-08 08:39:43 -0400

    updated finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,6 +54,7 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	qemu-aarch64 -L $TOOLCHAIN_PATH/aarch64-none-linux-gnu/libc writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	#writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
